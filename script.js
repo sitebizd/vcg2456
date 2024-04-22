26e1704419d5a8e8f2310603f81e5131f2de570f
sitebizd <vedantvu5@gmail.com>
2024-04-22 17:10:44 +0530

    Fire 1

--- script.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    var helloElement = document.getElementById('hello');
-    helloElement.textContent = 'Hello, ' + window._env_.USER_NAME;
-};
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
 
-app.get('/env.js', function(req, res) {
-    res.send('window._env_ = ' + JSON.stringify(process.env));
-});
+const firebase = require('firebase/app');
+require('firebase/storage');
 
-app.use(express.static(__dirname));
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
-});
+firebase.initializeApp(firebaseConfig);
