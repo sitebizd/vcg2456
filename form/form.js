9e661d0688c06a4905b87b15a905ecc2a784cae5
sitebizd <vedantvu5@gmail.com>
2024-05-22 16:53:18 +0530

    Form 25

--- form/form.js
+++ form/form.js
@@ -1,13 +1,24 @@
-document.getElementById('businessForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  var bname = document.getElementById('bname').value;
+function submitData() {
+  var businessName = document.getElementById('businessName').value;
   var address = document.getElementById('address').value;
   var category = document.getElementById('category').value;
-  var phone = document.getElementById('phone').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
   var description = document.getElementById('description').value;
 
-  google.script.run.submitForm(bname, address, category, phone, description);
+  var script_url = "https://script.google.com/macros/s/AKfycby50VF_4wKxrs23cRExjue5IKffXgPVu-acEW69Y6aE6-r7ksu_4hOUSAb4UFaWJoaNQQ/exec";
+  
+  var url = script_url+"?callback=ctrlq&businessName="+businessName+"&address="+address+"&category="+category+"&phoneNumber="+phoneNumber+"&description="+description+"&action=insert";
+  
+  var request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.onreadystatechange = function() {
+    if(request.readyState == 4 && request.status == 200) {
+      window.location.href = "thanks.html";
+    }
+  }
+  request.send();
+}
 
-  window.location.href = 'https://vedchitra.com/';
-});
+function ctrlq(e) {
+  console.log(e.result);
+}
